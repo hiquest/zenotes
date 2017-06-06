b2abaf53ace44e021ea69ec6bf3615537e0ed63e
Yanis <janis.sci@gmail.com>
2017-06-06 15:50:55 +0200

    Edited note should emerge to the top

--- src/js/App/index.js
+++ src/js/App/index.js
@@ -13,19 +13,25 @@ const component = {
   mounted: function() {
     const vm = this;
     loadNotes(this);
-    vm.$watch('selected', function(val, prev) {
+    vm.$watch(function() {
+      if (!this.selected) return undefined;
+      return { id: this.selected.id, body: this.selected.body };
+    }, function(val, prev) {
       if (!prev) return;
-      save(vm.notes);
+      if (val.id === prev.id && val.body !== prev.body) {
+        const ind = vm.notes.findIndex((x) => x.id === val.id);
+        if (ind > 0) {
+          const el = vm.notes[ind];
+          vm.notes.splice(ind, 1);
+          vm.notes = [el, ...vm.notes];
+        }
+        save(vm.notes);
+      }
     }, {
       deep: true
     });
   },
-  methods: { addNote, selectNote },
-  watch: {
-    notes: function() {
-      save(this.notes);
-    }
-  }
+  methods: { addNote, selectNote }
 };
 
 export default component;
@@ -34,14 +40,17 @@ function selectNote(note) {
   if (note === this.selected) return;
   this.selected = note;
 
-  this.notes = this.notes.filter((note) => !!note.body);
-  save(this.notes);
+  if (this.notes.some((n) => !n.body)) {
+    this.notes = this.notes.filter((note) => !!note.body);
+    save(this.notes);
+  }
 }
 
 function addNote() {
-  const note = { id: guid(), body: '#' };
+  const note = { id: guid(), body: '# ' };
   this.notes.unshift(note);
   this.selectNote(note);
+  save(this.notes);
 }
 
 function save(notes) {
