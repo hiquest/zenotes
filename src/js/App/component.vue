a6691c41b8b9a51f16788cdb8fc057e4bd095a8b
Yanis <janis.sci@gmail.com>
2017-06-07 08:46:24 +0200

    Add transitions

--- src/js/App/component.vue
+++ src/js/App/component.vue
@@ -3,13 +3,14 @@
   .column.column-20.sidebar
     h5.notes-title
       a(href='#' @click="addNote") +
-    ul
+    transition-group(name="flip-list" tag="ul")
       li(v-for="note in notes"
       :key="note.id"
       :class="{ active: note === selected }"
       @click="selectNote(note)") {{ note.body }}
   .column.column-80
-    editor(v-if='selected' v-model='selected.body' :key="selected.id")
+    transition(name="fade" appear)
+      editor(v-if='selected' v-model='selected.body' :key="selected.id")
 </template>
 
 <script src="./index.js"></script>
